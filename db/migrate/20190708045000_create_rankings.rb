026b5a66244c8dbb45b25c78e2f184b764b9e2b4
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-07-16 16:50:28 +0900

    Update 20190708045000_create_rankings.rb
    for heroku

--- db/migrate/20190708045000_create_rankings.rb
+++ db/migrate/20190708045000_create_rankings.rb
@@ -2,7 +2,8 @@ class CreateRankings < ActiveRecord::Migration[5.2]
   def change
     create_table :rankings do |t|
       t.string :name, null: false, unique: true, index: true
-      t.references :user, foreign_key: true
+      # t.references :user, foreign_key: true
+      t.string :user_id, null: false
       t.timestamps
     end
   end
